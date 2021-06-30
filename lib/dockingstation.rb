7c76b3f38918aac123a4eedc65debefc0e2121b5
Sibel Eren <edaeren575@gmail.com>
2021-06-30 17:16:36 +0100

    up to challenge 11

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,5 +1,14 @@
-class DockingStation
-  def release_bike
+require_relative 'bike'
 
+class DockingStation
+  attr_reader :bike
+
+  def release_bike
+    Bike.new
+  end
+
+
+  def dock(bike)
+    @bike = bike
   end
 end
---
+++ spec/bike_spec.rb
@@ -0,0 +1,6 @@
+require 'bike'
+require 'dockingstation'
+
+describe Bike do
+  it { is_expected.to respond_to :working? }
+end
